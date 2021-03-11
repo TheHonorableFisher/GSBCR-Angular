db9d7223c76de8d38aff6667010628574a9b2fc4
TheHonorableFisher <mael.venet@gmail.com>
2021-03-11 19:47:06 +0100

    Ajout des routes / suppression fichiers inutiles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { MedicamentComponent } from './medicament/medicament.component';
 import { MedecinsComponent } from './medecins/medecins.component';
 import { RapportsComponent } from './rapports/rapports.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 const appRoutes: Routes = [
-  {path : 'not-found',component:FourOhFourComponent},
+  // TODO ajouter une guard mais pour ça d'abord créer le système d'authentification
+  {path: 'accueil', component: AccueilComponent},
+  {path: 'medicaments', component: MedicamentComponent},
+  {path: 'medecins', component: MedecinsComponent},
+  {path: 'rapports', component: RapportsComponent},
   {path: '', component:ConnexionComponent},
+  {path : 'not-found',component:FourOhFourComponent},
   {path : '**', redirectTo : '/not-found'}
 ]
 
@@ -23,7 +29,8 @@ const appRoutes: Routes = [
     FourOhFourComponent,
     MedicamentComponent,
     MedecinsComponent,
-    RapportsComponent
+    RapportsComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule,
