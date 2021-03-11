2da69a8b8c26a2dae3bc316af8e34b15d2f480f0
TheHonorableFisher <mael.venet@gmail.com>
2021-03-11 17:22:44 +0100

    Ajout 404 + début route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  {path : 'not-found',component:FourOhFourComponent},
+  {path : '**', redirectTo : '/not-found'}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FourOhFourComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
